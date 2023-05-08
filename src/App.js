da72594cac93937337fdb9610bacc9c47880ec27
samKallel <samKallel@hotmail.frgit config --global user.email 'samKallel@hotmail.fr>
2023-05-08 21:49:30 +0100

    checkPoint fini

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+// les import comme d'habitude
 import React, { Component } from 'react'
 import Person from './Component/Person'
 
+// la classe App pour afficher le contenu de notre page
 export default class App extends Component {
+
+// declaration d'une variable show qui sert de temoin pour pouvoir afficher ou pas le componenent Person
   constructor(props){
     super(props);
     this.state = {show: false};
   }
+  // declaration de la fonction basculer qui va etre appelles a chaque fois que le bouton Toggle est clique
   basculer =() => this.setState({show: !this.state.show});
+  
   render() {
     return (
       <div>
-        
+ {/* le rendu du App qui est un bouton qui fait appel a la fonction bsculer pour changer le temoin  */}
         <button onClick={this.basculer}> Toggle </button>
+  {/* on teste si temoin (vrai) on affiche le component Person sinon on fait rien(null) */}
         {this.state.show? <Person/>: null}
-      
-        
+             
       </div>
     )
   }
--- src/Component/Person.js
+++ src/Component/Person.js
@@ -1,31 +1,50 @@
+// les importations: react, component, photo ( car elle est dans le dossier sorce) 
+// et le App.css pour la mise en page.
+
 import React, { Component } from 'react';
 import photo from "../img/my-image.png";
 import "../App.css";
 
+// on utilise les classe pour le composant App.js 
+
 export default class Person extends Component {
+
+    // le constructeur pour les declaration 
     constructor(props){
         super(props);
+      // l'objet personne est declare dynamiquement
+        this.personne = {
+          fullName: "Samiha Kallel",
+          bio: "Ingenieur en informatique",
+          profession: "Developpeur FS-JS",
+          imgProfile: photo
+          
+        }
+        // le compteur du temps ecoule
         this.state = {timeElapsed: 0 };
       }
-   
+// calculer le temps a l'aide de setInterval
+// on utilise la fonction predefinie componentDidMount pour activer le compteur une fois
+// le component Person est appele (monter dans le DOM)
     componentDidMount = () => {
       this.timer = setInterval(() => {
         this.setState({ timeElapsed: this.state.timeElapsed + 1 });
       }, 1000);
     }
+    // on efface le compteur une fois le composant Person n'est plus charge
     componentWillUnmount() {
       clearInterval(this.timer);
     }
-
+// le rendu de notre component Person le fulllName, le bio, imageProfile, 
+// profession et un compteur de temps
   render() {
     return (
       <div className='personnage'>
     <p> Temps ecoule {this.state.timeElapsed} seconds</p>
-      <h2>Samiha Kallel</h2>
-      <h3>Ingenieur en informatique</h3>  
-      <img src={photo} alt='maphoto'/>  
-      <h3>Developpeur</h3> 
-
+      <h2>{this.personne.fullName}</h2>
+      <h3>{this.personne.bio}</h3>
+      <img src= {this.personne.imgProfile} alt='maphoto'/>  
+      <h3>{this.personne.profession}</h3> 
       </div>
     )
   }
